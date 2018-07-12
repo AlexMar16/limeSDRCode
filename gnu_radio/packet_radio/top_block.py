dba64dbe2d7e23c1acaf1c544c7f4ec6ec60a7f0
James Robb <me@jamesrobb.ca>
2018-07-12 14:42:21 +0000

    commiting new code

---
+++ gnu_radio/packet_radio/top_block.py
@@ -0,0 +1,156 @@
+#!/usr/bin/env python2
+# -*- coding: utf-8 -*-
+##################################################
+# GNU Radio Python Flow Graph
+# Title: Top Block
+# Generated: Thu Jul  5 15:25:05 2018
+##################################################
+
+if __name__ == '__main__':
+    import ctypes
+    import sys
+    if sys.platform.startswith('linux'):
+        try:
+            x11 = ctypes.cdll.LoadLibrary('libX11.so')
+            x11.XInitThreads()
+        except:
+            print "Warning: failed to XInitThreads()"
+
+from PyQt4 import Qt
+from gnuradio import blocks
+from gnuradio import digital
+from gnuradio import eng_notation
+from gnuradio import gr
+from gnuradio.eng_option import eng_option
+from gnuradio.filter import firdes
+from grc_gnuradio import blks2 as grc_blks2
+from optparse import OptionParser
+import limesdr
+import sys
+from gnuradio import qtgui
+
+
+class top_block(gr.top_block, Qt.QWidget):
+
+    def __init__(self):
+        gr.top_block.__init__(self, "Top Block")
+        Qt.QWidget.__init__(self)
+        self.setWindowTitle("Top Block")
+        qtgui.util.check_set_qss()
+        try:
+            self.setWindowIcon(Qt.QIcon.fromTheme('gnuradio-grc'))
+        except:
+            pass
+        self.top_scroll_layout = Qt.QVBoxLayout()
+        self.setLayout(self.top_scroll_layout)
+        self.top_scroll = Qt.QScrollArea()
+        self.top_scroll.setFrameStyle(Qt.QFrame.NoFrame)
+        self.top_scroll_layout.addWidget(self.top_scroll)
+        self.top_scroll.setWidgetResizable(True)
+        self.top_widget = Qt.QWidget()
+        self.top_scroll.setWidget(self.top_widget)
+        self.top_layout = Qt.QVBoxLayout(self.top_widget)
+        self.top_grid_layout = Qt.QGridLayout()
+        self.top_layout.addLayout(self.top_grid_layout)
+
+        self.settings = Qt.QSettings("GNU Radio", "top_block")
+        self.restoreGeometry(self.settings.value("geometry").toByteArray())
+
+        ##################################################
+        # Variables
+        ##################################################
+        self.samp_rate = samp_rate = 400000
+
+        ##################################################
+        # Blocks
+        ##################################################
+        self.limesdr_sink_0 = limesdr.sink('0009070602433917',
+        		       2,
+        		       1,
+        		       0,
+        		       0,
+        		       '',
+        		       860e6,
+        		       samp_rate,
+        		       0,
+        		       0,
+        		       10e6,
+        		       0,
+        		       10e6,
+        		       1,
+        		       1,
+        		       1,
+        		       1,
+        		       6e6,
+        		       1,
+        		       5e6,
+        		       0,
+        		       0,
+        		       0,
+        		       0,
+        		       30,
+        		       30,
+        		       0,
+        		       0,
+        		       0,
+        		       0)
+        self.digital_gmsk_mod_0 = digital.gmsk_mod(
+        	samples_per_symbol=2,
+        	bt=0.35,
+        	verbose=False,
+        	log=False,
+        )
+        self.blocks_throttle_0 = blocks.throttle(gr.sizeof_float*1, samp_rate,True)
+        self.blocks_file_source_0 = blocks.file_source(gr.sizeof_float*1, '/home/doctor/harro.txt', True)
+        self.blks2_packet_encoder_0 = grc_blks2.packet_mod_f(grc_blks2.packet_encoder(
+        		samples_per_symbol=1,
+        		bits_per_symbol=1,
+        		preamble='',
+        		access_code='1100110011',
+        		pad_for_usrp=True,
+        	),
+        	payload_length=0,
+        )
+
+        ##################################################
+        # Connections
+        ##################################################
+        self.connect((self.blks2_packet_encoder_0, 0), (self.digital_gmsk_mod_0, 0))
+        self.connect((self.blocks_file_source_0, 0), (self.blocks_throttle_0, 0))
+        self.connect((self.blocks_throttle_0, 0), (self.blks2_packet_encoder_0, 0))
+        self.connect((self.digital_gmsk_mod_0, 0), (self.limesdr_sink_0, 0))
+
+    def closeEvent(self, event):
+        self.settings = Qt.QSettings("GNU Radio", "top_block")
+        self.settings.setValue("geometry", self.saveGeometry())
+        event.accept()
+
+    def get_samp_rate(self):
+        return self.samp_rate
+
+    def set_samp_rate(self, samp_rate):
+        self.samp_rate = samp_rate
+        self.blocks_throttle_0.set_sample_rate(self.samp_rate)
+
+
+def main(top_block_cls=top_block, options=None):
+
+    from distutils.version import StrictVersion
+    if StrictVersion(Qt.qVersion()) >= StrictVersion("4.5.0"):
+        style = gr.prefs().get_string('qtgui', 'style', 'raster')
+        Qt.QApplication.setGraphicsSystem(style)
+    qapp = Qt.QApplication(sys.argv)
+
+    tb = top_block_cls()
+    tb.start()
+    tb.show()
+
+    def quitting():
+        tb.stop()
+        tb.wait()
+    qapp.connect(qapp, Qt.SIGNAL("aboutToQuit()"), quitting)
+    qapp.exec_()
+
+
+if __name__ == '__main__':
+    main()
--- limesdr_read_samples/main.c
+++ limesdr_read_samples/main.c
@@ -3,28 +3,11 @@
 #include <stdio.h> //printf
 #include <stdlib.h> //free
 #include <complex.h>
-#include <math.h> // sqrt, pow
 
-struct SoapySDRDevice *Setup(void);
-void DeviceInfo(struct SoapySDRDevice *sdr);
-void Read_1024_samples(struct SoapySDRDevice *sdr, double freq, complex float *buffer);
-void Close(struct SoapySDRDevice *sdr2);
-
-const double MAX_FREQ = 3.8e9;
-const double Min_FREQ = 10e9;
-
-size_t length;
-
-int main(int argc, char *argv[])
+int main(void)
 {
-    complex float *buf;
-    struct SoapySDRDevice *sdr = Setup();
-    DeviceInfo(sdr);
-    Read_1024_samples(sdr, 433.92e6, buf);
-    Close(sdr);
-}
+    size_t length;
 
-struct SoapySDRDevice *Setup(void) {
     //enumerate devices
     SoapySDRKwargs *results = SoapySDRDevice_enumerate(NULL, &length);
     for (size_t i = 0; i < length; i++)
@@ -50,11 +33,8 @@ struct SoapySDRDevice *Setup(void) {
         printf("SoapySDRDevice_make fail: %s\n", SoapySDRDevice_lastError());
         return EXIT_FAILURE;
     }
-    return sdr;
-}
 
-void DeviceInfo(struct SoapySDRDevice *sdr) {
-     //query device info
+    //query device info
     char** names = SoapySDRDevice_listAntennas(sdr, SOAPY_SDR_RX, 0, &length);
     printf("Rx antennas: ");
     for (size_t i = 0; i < length; i++) printf("%s, ", names[i]);
@@ -72,24 +52,13 @@ void DeviceInfo(struct SoapySDRDevice *sdr) {
     for (size_t i = 0; i < length; i++) printf("[%g Hz -> %g Hz], ", ranges[i].minimum, ranges[i].maximum);
     printf("\n");
     free(ranges);
-}
 
-
-void Read_1024_samples(struct SoapySDRDevice *sdr, double freq, complex float *buffer) {
-    // check freq is within range
-    if( freq < 10e3) {
-        freq = 10e3;
-        printf("Frequency out of range setting min: \n");
-    }
-    if ( freq > MAX_FREQ) {
-        freq = MAX_FREQ;
-    }
     //apply settings
     if (SoapySDRDevice_setSampleRate(sdr, SOAPY_SDR_RX, 0, 1e6) != 0)
     {
         printf("setSampleRate fail: %s\n", SoapySDRDevice_lastError());
     }
-    if (SoapySDRDevice_setFrequency(sdr, SOAPY_SDR_RX, 0, freq, NULL) != 0)
+    if (SoapySDRDevice_setFrequency(sdr, SOAPY_SDR_RX, 0, 433.92e6, NULL) != 0)
     {
         printf("setFrequency fail: %s\n", SoapySDRDevice_lastError());
     }
@@ -104,49 +73,24 @@ void Read_1024_samples(struct SoapySDRDevice *sdr, double freq, complex float *b
 
     //create a re-usable buffer for rx samples
     complex float buff[1024];
-    float min, max;
-    min = (float)INT32_MAX;
-    max = 0;
-
-    int mincounter, maxcounter;
-
-    mincounter = maxcounter = 0;
 
     //receive some samples
-    for (size_t i = 0; i < 10000; i++)
+    for (size_t i = 0; i < 10; i++)
     {
         void *buffs[] = {buff}; //array of buffers
         int flags; //flags set by receive operation
         long long timeNs; //timestamp for receive buffer
         int ret = SoapySDRDevice_readStream(sdr, rxStream, buffs, 1024, &flags, &timeNs, 100000);
-        // printf("ret=%d, flags=%d, timeNs=%lld\n", ret, flags, timeNs);
-        fflush(stdout);
-        for(size_t j = 0; j < ret; j++) {
-            float fr = creal(buff[j]);
-            float fi = cimag(buff[j]);
-            float amplitude = sqrt(pow(fr, 2) + pow(fi, 2));
-            // printf("amplitude: %f \n", amplitude);
-            if (amplitude < min) {
-                min = amplitude;
-                mincounter += 1;
-            } else if(amplitude > max) {
-                max = amplitude;
-                maxcounter += 1;
-            }
-        }
-        // printf("min:%f | max:%f \n", min, max);
+        printf("ret=%d, flags=%d, timeNs=%lld\n", ret, flags, timeNs);
     }
-    printf("max: %f | min: %f \n", max, min);
-    printf("We took 10.000 times 1024 samples and out of that the \n maxcounter is: %d \n and the  \nmincounter is: %d \n", maxcounter, mincounter);
 
     //shutdown the stream
     SoapySDRDevice_deactivateStream(sdr, rxStream, 0, 0); //stop streaming
     SoapySDRDevice_closeStream(sdr, rxStream);
-}
 
-void Close(struct SoapySDRDevice *sdr2) {
     //cleanup device handle
-    SoapySDRDevice_unmake(sdr2);
+    SoapySDRDevice_unmake(sdr);
+
     printf("Done\n");
-    //return EXIT_SUCCESS;
+    return EXIT_SUCCESS;
 }
